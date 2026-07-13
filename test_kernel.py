83904a3791afee7b01aa789cb482243a05e97d07
Francesco Leveque <syraele@yahoo.it>
2026-07-13 18:10:26 +0200

    build: configure project packaging and virtual environment

--- test_kernel.py
+++ test_kernel.py
@@ -1,8 +1,8 @@
-import sys
-from pathlib import Path
+
+
 
 # Aggiunge la cartella "src" al percorso di ricerca dei moduli
-sys.path.insert(0, str(Path(__file__).parent / "src"))
+
 
 from edge.core.kernel import Kernel
 
